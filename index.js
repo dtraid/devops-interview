1f18101a9899540c568896d15396943b3d336f5c
dtraid <diego@dtraid.com>
2022-03-22 20:15:45 +0100

    Comment and clean

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 import { writeCsvFile } from './libs/csv.js';
-import { orderCosts, productCustomers, customerRanking } from './libs/process.js';
+import { listOrderCosts, listCustomerRanking, listCustomersByProduct } from './libs/process.js';
 
-// Task 1: Cost of each order
-writeCsvFile('./files/order_prices.csv', orderCosts);
+// Task 1: List the cost of each order
+writeCsvFile('./files/order_prices.csv', listOrderCosts);
 
 // Task 2: Customers that have purchased each product
-writeCsvFile('./files/product_customers.csv', productCustomers);
+writeCsvFile('./files/product_customers.csv', listCustomersByProduct);
 
 // Task 3: Ranking of customers by spending
-writeCsvFile('./files/customer_ranking.csv', customerRanking);
+writeCsvFile('./files/customer_ranking.csv', listCustomerRanking);
 
 console.log(`
 ⣿⣿⣿⣿⣿⠟⠉⠁⠄⠄⠄⠈⠙⠿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
--- libs/process.js
+++ libs/process.js
@@ -4,40 +4,53 @@ const customers = await readCsvFile('./files/customers.csv');
 const rawProducts = await readCsvFile('./files/products.csv');
 const rawOrders = await readCsvFile('./files/orders.csv');
 
+// Convert cost to Number
 const products = rawProducts.map((product) => ({ ...product, cost: parseFloat(product.cost) }));
 
+// Convert space-separated product ids to array of product ids
 const orders = rawOrders.map((order) => ({
   ...order,
   products: order.products.split(/\s+/),
 }));
 
-const findProduct = (productId) => products.find((product) => product.id === productId);
-const productPrice = (productId) => findProduct(productId).cost;
-const productsCost = (productIds) =>
-  productIds.reduce((totalCost, productId) => totalCost + productPrice(productId), 0);
+// Find product by id
+const getProduct = (productId) => products.find((product) => product.id === productId);
 
-// Task 2
-const orderCosts = orders.map((order) => ({
+// Get price of a product
+const getPrice = (productId) => getProduct(productId).cost;
+
+// Calculate the cost of a list of products
+const getProductsCost = (productIdsArr) =>
+  productIdsArr.reduce((totalCost, productId) => totalCost + getPrice(productId), 0);
+
+// Calculate the cost of all orders (task 1)
+const listOrderCosts = orders.map((order) => ({
   id: order.id,
-  euros: productsCost(order.products),
+  euros: getProductsCost(order.products),
 }));
 
-const productCustomers = products.map((product) => {
-  const customerIds = orders.reduce(
-    (acc, order) => (order.products.includes(product.id) ? acc.add(order.customer) : acc),
+// Find all the customers that have purchased a specific product
+const getCustomersByProduct = (productId) =>
+  orders.reduce(
+    (customers, order) =>
+      order.products.includes(productId) ? customers.add(order.customer) : customers,
     new Set()
   );
 
-  return { id: product.id, customer_ids: [...customerIds].join(' ') };
-});
+// List customers that have purchased each product
+const listCustomersByProduct = products.map((product) => ({
+  id: product.id,
+  customer_ids: [...getCustomersByProduct(product.id)].join(' '),
+}));
 
-const customerTotal = (customer) =>
+const getCustomerTotal = (customer) =>
   orders
     .filter((order) => order.customer === customer.id)
-    .reduce((acc, order) => acc + productsCost(order.products), 0);
+    .reduce((acc, order) => acc + getProductsCost(order.products), 0);
 
-const customerRanking = customers
-  .map((customer) => ({ ...customer, total_euros: customerTotal(customer) }))
+// Rank customers by spending (task 3)
+const listCustomerRanking = customers
+  .map((customer) => ({ ...customer, total_euros: getCustomerTotal(customer) }))
   .sort((a, b) => b.total_euros - a.total_euros);
 
-export { orderCosts, productCustomers, customerRanking };
+export { listOrderCosts, listCustomersByProduct, listCustomerRanking };
