bab2c4c2194a33af3d808f79409fb39120f876b2
dtraid <diego@dtraid.com>
2022-03-22 17:55:11 +0100

    Add task 1 draft

---
+++ 1.js
@@ -0,0 +1,23 @@
+import { readCsvFile, writeCsvFile } from './csv.js';
+
+const products = await readCsvFile('./products.csv');
+const rawOrders = await readCsvFile('./orders.csv');
+
+const orders = rawOrders.map((order) => ({
+  ...order,
+  products: order.products.split(/\s+/),
+}));
+
+const totalOrderCost = (productIds) =>
+  productIds.reduce((acc, productId) => {
+    const price = products.find((product) => product.id === productId).cost;
+
+    return acc + parseFloat(price);
+  }, 0);
+
+const orderCosts = orders.map((order) => ({
+  id: order.id,
+  euros: totalOrderCost(order.products),
+}));
+
+writeCsvFile('./order_costs.csv', orderCosts);
